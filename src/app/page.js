a0bbf4fa8168b6d04fbd00feee1873009a68ce13
anurag-17 <anurag.pandey17@gmail.com>
2023-12-05 19:43:33 +0530

    corrections

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,9 @@ import Link from "next/link";
 import Image from "next/image";
 import misa from "../../public/misa.svg";
 import srch from "../../public/search.svg";
+import search from "../../public/Union.svg";
+import user from "../../public/Vector (1).svg";
+import basket from "../../public/Vector (2).svg";
 import p1 from "../../public/P1.svg";
 import p2 from "../../public/P2.svg";
 import p3 from "../../public/P3.svg";
@@ -12,10 +15,10 @@ import p6 from "../../public/P6.svg";
 import p7 from "../../public/P7.png";
 import p8 from "../../public/P8.png";
 import p9 from "../../public/P9.png";
-import g1 from "../../public/Group 31.png";
-import g2 from "../../public/Group 29.png";
-import g3 from "../../public/Group 28.png";
-import g4 from "../../public/Group 30.png";
+import g1 from "../../public/Group 31.svg";
+import g2 from "../../public/Group 29.svg";
+import g3 from "../../public/Group 28.svg";
+import g4 from "../../public/Group 30.svg";
 import r1 from "../../public/Rectangle 18.svg";
 import r2 from "../../public/Rectangle 19.svg";
 import r3 from "../../public/Rectangle 20.svg";
@@ -36,33 +39,52 @@ const Page = () => {
     <>
       <section>
         <div className="container-fluid" id="family">
-          <div className="flex justify-center mx-24 2xl:my-5 xl:my-2 lg:my-1">
+        <div className="fixed  
+                w-full top-0 left-0 bg-white">
+          <div className="py-4 flex items-center justify-center mx-24 2xl:my-5 xl:my-2 lg:my-1 relative">
             <Image
               src={misa}
               alt="image"
-              className="ml-auto 2xl:w-52 xl:w-36 lg:w-32"
+              className="absolute 2xl:w-52 xl:w-36 lg:w-32 "
             />
-            
-            {/* <Image
-              src={srch}
-              alt="image"
-              className="ml-auto 2xl:w-44 xl:w-24 lg:w-20"
-            /> */}
+            <div className="flex 2xl:gap-9 ml-auto ">
+              <Image className="cursor-pointer w-6" src={search} alt="image" />
+              <Image className="cursor-pointer w-6" src={user} alt="image" />
+              <Image className="cursor-pointer w-6" src={basket} alt="image" />
+            </div>
           </div>
+
           <hr className="border-t-2 border-[#315031] opacity-25" />
           <div className="meanu lg:block hidden ">
             <ul className="flex justify-center lg:text-[15px] xl:text-xs 2xl:text-base lg:space-x-4 xl:space-x-7 2xl:space-x-16   py-3 ">
-              <li>HOME AND NEWS</li>
-              <li>BESTSELLERS</li>
-              <li>COMPLEXION</li>
-              <li>EYES</li>
-              <li>LIPS</li>
-              <li>CARE</li>
-              <li>MAKEUP SESSION</li>
-              <li>SERVICES</li>
+              <li> <Link href="#" className="hover:underline">
+              HOME AND NEWS
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              BESTSELLERS
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              COMPLEXION
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              EYES
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              LIPS
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              CARE
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              MAKEUP SESSION
+                    </Link></li>
+              <li><Link href="#" className="hover:underline">
+              SERVICES
+                    </Link></li>
             </ul>
           </div>
-          <div id="main-img" className="flex 2xl:pt-72 xl:pt-52 lg:pt-40">
+          </div>
+          <div id="main-img" className="z-50 mt-32 flex 2xl:pt-72 xl:pt-52 lg:pt-40">
             <div className="lg:px-16 xl:px-24 2xl:px-28  middle-text">
               <div className="lg:text-3xl xl:text-4xl 2xl:text-6xl lg:pb-1 xl:pb-2 2xl:pb-4 bright fontf">
                 BRIGHT
@@ -82,13 +104,23 @@ const Page = () => {
           <div className="flex justify-around lg:px-16 xl:px-24 2xl:px-28 gap-1 mt-16">
             <div className="w-1/3">
               <div id="purple">
-                <Image className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9" src={p1} alt="image" />
+                <Image
+                  className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9"
+                  src={p1}
+                  alt="image"
+                />
               </div>
-              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">CORRECTORS</h1>
+              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
+                CORRECTORS
+              </h1>
             </div>
             <div className="w-1/3">
               <div id="purple">
-                <Image className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-12" src={p2} alt="image" />
+                <Image
+                  className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-12"
+                  src={p2}
+                  alt="image"
+                />
               </div>
               <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
                 PERLY EYES COLLECTION
@@ -96,16 +128,26 @@ const Page = () => {
             </div>
             <div className="w-1/3">
               <div id="purple">
-                <Image className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9" src={p3} alt="image" />
+                <Image
+                  className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9"
+                  src={p3}
+                  alt="image"
+                />
               </div>
-              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">BLUSH BLUSH</h1>
+              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
+                BLUSH BLUSH
+              </h1>
             </div>
           </div>
 
           <div className="flex justify-around lg:px-16 xl:px-24 2xl:px-28 gap-1 lg:mt-7 xl:mt-10 2xl:mt-16">
             <div className="w-1/3">
               <div id="purple">
-                <Image className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9" src={p4} alt="image" />
+                <Image
+                  className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9"
+                  src={p4}
+                  alt="image"
+                />
               </div>
               <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
                 SATIN LIPSTICKS
@@ -113,37 +155,59 @@ const Page = () => {
             </div>
             <div className="w-1/3">
               <div id="purple">
-                <Image className="mx-auto  lg:h-80 xl:h-full lg:py-7 xl:py-9" src={p5} alt="image" />
+                <Image
+                  className="mx-auto  lg:h-80 xl:h-full lg:py-7 xl:py-9"
+                  src={p5}
+                  alt="image"
+                />
               </div>
-              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">FOUNDATIONS</h1>
+              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
+                FOUNDATIONS
+              </h1>
             </div>
             <div className="w-1/3">
               <div id="purple">
-                <Image className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9" src={p6} alt="image" />
+                <Image
+                  className="mx-auto lg:h-80 xl:h-full lg:py-7 xl:py-9"
+                  src={p6}
+                  alt="image"
+                />
               </div>
-              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">EYES</h1>
+              <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
+                EYES
+              </h1>
             </div>
           </div>
           {/* --------section 3------ */}
-       <div className="px-28 mt-16" id="box1">
-        <div className="flex flex-col items-center lg:py-32 xl:py-36 2xl:py-72 text-white">
-          <div className="fontf  lg:text-[35px] xl:text-[40px] 2xl:text-[54px]">PERLAY EYES</div>
-          <div className="fontf lg:text-[35px] xl:text-[40px] 2xl:text-[54px]">COLLECTION</div>
-          <div>
-         <button className="lg:mr-[80px] xl:mr-[120px] 2xl:mr-52 lg:text-[14px] xl:text-[16px] lg:px-4 xl:px-6 lg:py-2 xl:py-3 lg:mt-5 xl:mt-8 border border-white rounded-full">SHOP NOW</button>
-        </div></div>
-       </div>
-
+          <div className="px-28 mt-16" id="box1">
+            <div className="flex flex-col items-center lg:py-32 xl:py-36 2xl:py-72 text-white">
+              <div className="fontf  lg:text-[35px] xl:text-[40px] 2xl:text-[54px]">
+                PERLAY EYES
+              </div>
+              <div className="fontf lg:text-[35px] xl:text-[40px] 2xl:text-[54px]">
+                COLLECTION
+              </div>
+              <div>
+                <button className="lg:mr-[80px] xl:mr-[120px] 2xl:mr-52 lg:text-[14px] xl:text-[16px] lg:px-4 xl:px-6 lg:py-2 xl:py-3 lg:mt-5 xl:mt-8 border border-white rounded-full">
+                  SHOP NOW
+                </button>
+              </div>
+            </div>
+          </div>
 
           {/* ---------SECTION 5---------- */}
-          <div className="lg:text-3xl  xl:text-4xl 2xl:text-5xl lg:mt-16 xl:mt-20 2xl:mt-28 flex justify-center fontf">
+          <div className="lg:text-3xl  xl:text-4xl 2xl:text-5xl lg:mt-16 xl:mt-20 2xl:mt-10 flex justify-center fontf">
             NEW ARRIVAL
           </div>
 
           <div className="flex justify-around lg:px-16 xl:px-24 2xl:px-28 gap-1 mt-16">
             <div className="w-1/3">
               <div id="voilets">
-                <Image className="mx-auto py-12 lg:h-96 2xl:h-full" src={p7} alt="image" />
+                <Image
+                  className="mx-auto py-12 lg:h-96 2xl:h-full"
+                  src={p7}
+                  alt="image"
+                />
               </div>
               <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
                 SMOOTHING SMOOTHING BASE
@@ -151,7 +215,11 @@ const Page = () => {
             </div>
             <div className="w-1/3">
               <div id="voilets">
-                <Image className="mx-auto py-10 lg:h-96 2xl:h-full" src={p8} alt="image" />
+                <Image
+                  className="mx-auto py-10 lg:h-96 2xl:h-full"
+                  src={p8}
+                  alt="image"
+                />
               </div>
               <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
                 EYE LINER LIKER BLACK
@@ -159,7 +227,11 @@ const Page = () => {
             </div>
             <div className="w-1/3">
               <div id="voilets">
-                <Image className="mx-auto py-9 lg:h-96 2xl:h-full" src={p9} alt="image" />
+                <Image
+                  className="mx-auto py-9 lg:h-96 2xl:h-full"
+                  src={p9}
+                  alt="image"
+                />
               </div>
               <h1 className="flex justify-center xl:text-lg 2xl:text-xl my-5">
                 LIPLIKER LIPSTICK MISAPARIS
@@ -168,12 +240,34 @@ const Page = () => {
           </div>
 
           {/* ----------SECTION 6---------- */}
+         <div className="lg:px-16 xl:px-24 2xl:px-28 mt-20">
+         <div>
+          
+         </div>
+         </div>
+
           <div className="lg:px-16 xl:px-24 2xl:px-28 mt-20">
             <div className="flex justify-between lg:py-12 xl:py-16 lg:px-16 xl:px-20  bg-black">
-              <Image className="lg:w-40 xl:w-56  2xl:w-full" src={g1} alt="image" />
-              <Image className="lg:w-40 xl:w-56  2xl:w-full" src={g2} alt="image" />
-              <Image className="lg:w-40 xl:w-56  2xl:w-full" src={g3} alt="image" />
-              <Image className="lg:w-40 xl:w-56  2xl:w-full" src={g4} alt="image" />
+              <Image
+                className="lg:w-40 xl:w-56  2xl:w-64"
+                src={g1}
+                alt="image"
+              />
+              <Image
+                className="lg:w-40 xl:w-56  2xl:w-64"
+                src={g2}
+                alt="image"
+              />
+              <Image
+                className="lg:w-40 xl:w-56  2xl:w-64"
+                src={g3}
+                alt="image"
+              />
+              <Image
+                className="lg:w-40 xl:w-56  2xl:w-64"
+                src={g4}
+                alt="image"
+              />
             </div>
           </div>
           {/* ----------section 7---------- */}
@@ -252,11 +346,11 @@ const Page = () => {
                 <div className="space-y-3 w-[25%]">
                   <Image className="mb-5" src={misa} alt="image" />
 
-                  <p className="lg:text-xs xl:text-sm 2xl:text-sm">
+                  <p className="lg:text-xs xl:text-sm 2xl:text-base">
                     Our products are certified “guaranteed French origin”
                     <br /> and environmentally friendly.
                   </p>
-                  <p className="lg:text-xs xl:text-sm 2xl:text-sm">
+                  <p className="lg:text-xs xl:text-sm 2xl:text-base">
                     All of our makeup products are formulated and manufactured
                     in France with natural ingredients and using sustainable
                     processes. Very high quality pigments.
@@ -285,7 +379,7 @@ const Page = () => {
                   <h2 className="lg:text-lg xl:text-xl mb-6 uppercase fontf ">
                     QUICK LINKS
                   </h2>
-                  <ul className="xl:text-sm 2xl:text-sm dark:text-gray-400 lg:space-y-2 xl:space-y-2 2xl:space-y-4 ">
+                  <ul className="xl:text-sm 2xl:text-base dark:text-gray-400 lg:space-y-2 xl:space-y-2 2xl:space-y-3 ">
                     <li className="">
                       <Link href="#" className="hover:underline">
                         Home And News
@@ -333,7 +427,7 @@ const Page = () => {
                   <h2 className="mb-6   uppercase fontf text-xl">
                     CUSTOMER SERVICES
                   </h2>
-                  <ul className="xl:text-sm 2xl:text-sm dark:text-gray-400 lg:space-y-2 xl:space-y-2 2xl:space-y-4 ">
+                  <ul className="xl:text-sm 2xl:text-base dark:text-gray-400 lg:space-y-2 xl:space-y-2 2xl:space-y-3 ">
                     <li className="">
                       <Link href="#" className="hover:underline">
                         Delivery & Return
@@ -357,7 +451,7 @@ const Page = () => {
                   <input
                     className="border py-2 w-full"
                     type="email"
-                    placeholder="Email"
+                    placeholder=" EUR € | France"
                   />
                 </div>
               </div>
